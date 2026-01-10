41e5e078985e0fe209b64b4c5368b3ea98b2bd01
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-10 11:21:11 +0100

    fix: correct AutoService annotation to use SurfProfileApi instead of SurfPlaytimeApi

--- surf-profile-paper/src/main/kotlin/dev/slne/surf/profile/paper/api/SurfProfileApiImpl.kt
+++ surf-profile-paper/src/main/kotlin/dev/slne/surf/profile/paper/api/SurfProfileApiImpl.kt
@@ -1,14 +1,13 @@
 package dev.slne.surf.profile.paper.api
 
 import com.google.auto.service.AutoService
-import dev.slne.surf.playtime.api.SurfPlaytimeApi
 import dev.slne.surf.profile.api.SurfProfileApi
 import dev.slne.surf.profile.paper.menu.own.ownProfileMenu
 import net.kyori.adventure.util.Services
 import org.bukkit.Bukkit
 import java.util.*
 
-@AutoService(SurfPlaytimeApi::class)
+@AutoService(SurfProfileApi::class)
 class SurfProfileApiImpl : SurfProfileApi, Services.Fallback {
     override fun openOwnProfileMenu(playerUuid: UUID) {
         ownProfileMenu(Bukkit.getPlayer(playerUuid) ?: error("Player not found"))
