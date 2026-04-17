b45fc5cc40838c995b551eb65ae615ba0aef1d7d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-17 19:16:23 +0200

    refactor: change parameter type from HumanEntity to Player in openMenu function

--- surf-profile-paper/src/main/kotlin/dev/slne/surf/profile/paper/integration/SettingsIntegration.kt
+++ surf-profile-paper/src/main/kotlin/dev/slne/surf/profile/paper/integration/SettingsIntegration.kt
@@ -1,10 +1,10 @@
 package dev.slne.surf.profile.paper.integration
 
 import dev.slne.surf.settings.api.SurfSettingsApi
-import org.bukkit.entity.HumanEntity
+import org.bukkit.entity.Player
 
 object SettingsIntegration {
-    fun openMenu(player: HumanEntity) {
+    fun openMenu(player: Player) {
         SurfSettingsApi.openSettingsGui(player.uniqueId)
     }
 }
